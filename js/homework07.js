ff544dd2a6f7d94ac68b19aca2280e0812ecd740
saimang12@naver.com <saimang12@naver.com>
2023-03-20 16:42:40 +0900

    2023-03-20

--- js/homework07.js
+++ js/homework07.js
@@ -1,34 +1,57 @@
 $(function () {
 
-
-    // header 탭열기
-    $('.header .h_icon').on('click', function () {
-        $('.right_tap').addClass('on')
-    });
-
-    // header 탭닫기
-    $('.right_tap .close_box').on('click', function () {
-        $('.right_tap').removeClass('on')
-    });
-
-
-
-
-    // subVisual 슬라이드 옵션
-    $('.subVisual .slide_box').slick({
+    const MainSlideOption = {
         arrows: false,
         fade: true,
+    }
+
+    $('.mainSlide').slick(MainSlideOption);
+
+    $('.mainVisual .arrows .left').on('click', function () {
+        $('.mainSlide').slick('slickPrev');
+    });
+    $('.mainVisual .arrows .right').on('click', function () {
+        $('.mainSlide').slick('slickNext');
     });
 
 
 
+    const DressOption = {
+        // centerMode: true,
+        // centerPadding: '300px',
+        arrows: false,
+        slidesToShow: 3,
+    };
 
+    $('.Dress').slick(DressOption);
 
-    // slideSection 슬라이드 옵션 
-    $('.slideSection .slide_box').slick({
-        slidesToShow: 2,
-        slidesToScroll: 2,
+    $('.dress_arrows .left').on('click', function () {
+        $('.Dress').slick('slickPrev');
     });
+
+    $('.dress_arrows .right').on('click', function () {
+        $('.Dress').slick('slickNext');
+    });
+
+
+
+    const SuitOption = {
+        // centerMode: true,
+        // centerPadding: '300px',
+        arrows: false,
+        slidesToShow: 3,
+    };
+
+    $('.Suit').slick(SuitOption);
+
+    $('.suit_arrows .left').on('click', function () {
+        $('.Suit').slick('slickPrev');
+    });
+
+    $('.suit_arrows .right').on('click', function () {
+        $('.Suit').slick('slickNext');
+    });
+
 
 
 
